fe8fe63d8c38edef5b9977aee0349087a318746d
PaulLubnin <egopaul@inbox.ru>
2019-08-13 13:30:00 +0500

    исправил генератор

--- Generate MD5.py
+++ Generate MD5.py
@@ -1,19 +1,11 @@
 import hashlib
 
 def hash_generate(file_path):
-    """
-    Не смог реализовать "красивый" выход из цикла, когда пустая строка останавливает его
-    Пытался привязать выход к самой строке, но в таком случае генератор возвращает строки через одну, т.к.
-        используется .readline()
-    while len(text_file.readline().strip()) == 0
-    """
     with open(file_path, 'rb') as text_file:
-        while True:
-            yield hashlib.md5(text_file.readline().strip()).hexdigest()
+        for line in text_file:
+            yield hashlib.md5(line).hexdigest()
 
 
 print(hash_generate('country_links.txt'))
 for line in hash_generate('country_links.txt'):
     print(line)
-    if line == 'd41d8cd98f00b204e9800998ecf8427e':
-        break
